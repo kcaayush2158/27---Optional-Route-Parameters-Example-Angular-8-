de0c229fb111cdd9bd5f765e9e74950096914596
aayush kc <kcaayush2158@gmail.com>
2019-06-05 23:08:12 +0545

    Project Uploaded

---
+++ src/app/department-detail/department-detail.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router, ParamMap} from'@angular/router';
+
+import { from } from 'rxjs';
+@Component({
+  selector: 'app-department-detail',
+  template: `
+  
+    <p>
+    you selected the   department-detail with id = {{departmentId}}
+    </p>
+    <button (click)="goNext()">Next</button><br>
+    <button (click)="goPrevious()">Previous</button>
+
+    <div>
+    <button (click)="gotoDepartments()">Back</button>
+    </div>
+  `,
+  styles: []
+})
+export class DepartmentDetailComponent implements OnInit {
+  public departmentId;
+  
+  constructor(private activatedRouter : ActivatedRoute, private router : Router) { }
+
+  ngOnInit() {
+this.activatedRouter.paramMap.subscribe((params:ParamMap)=>{
+let id = parseInt(params.get('id'));
+this.departmentId= id;
+});
+
+  }
+  goPrevious(){
+    let previousId = this.departmentId - 1;
+    this.router.navigate(['/departments',previousId]);
+
+  }
+  goNext(){
+    let nextId  =  this.departmentId + 1;
+    this.router.navigate(['/departments',nextId]);
+
+  }
+  gotoDepartments(){
+    let selectedId= this.departmentId ? this.departmentId : null;
+    this.router.navigate(['/departments',{id:selectedId}]);
+
+  }
+}
--- src/app/department-list/department-list.component.ts
+++ src/app/department-list/department-list.component.ts
@@ -1,19 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 @Component({
   selector: 'app-department-list',
   template: `
-    <p>
-      department-list works!
-    </p>
+   <h3>
+      Department List
+    </h3>
+    <ul class="items">
+       <li [class.selected]="isSelected(department)" (click)="onSelect()" *ngFor="let department of departments">
+           {{department.id}} {{department.name}}<br>
+       </li>
+    </ul>
   `,
   styles: []
 })
 export class DepartmentListComponent implements OnInit {
+public selectedId;
+  public departments =[
+{"id":1,"name":"Angular"},
+{"id":2,"name":"Node"},
+{"id":3,"name":"Java"},
+{"id":4,"name":"MangoDB"},
+{"id":5,"name":"Bootstrap"}
+  ]
+  public departmentId;
 
-  constructor() { }
+  constructor(private router : Router,private route:ActivatedRoute ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params:ParamMap) => {
+      let id = parseInt(params.get('id'));
+      this.selectedId= id;
+    });
   }
 
+
+
+  onSelect(department){
+    this.router.navigate(['/departments',department.id]);
+  }
+
+isSelected(department){
+  return department.id === this.selectedId;
+}
+
+
+ 
 }
